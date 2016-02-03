bcd3b059930986a5859c64865bd82003cf163f9d
Sellto <selleslagh.tom@gmail.com>
2016-02-03 15:54:56 +0100

    Fix Utils module to pass test

--- test_utils.py
+++ test_utils.py
@@ -7,16 +7,18 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-        self.assertEqual(utils.fact(2),2)
-        self.assertEqual(utils.fact(-2),-1)
-        pass
+        self.assertEqual(utils.fact(3),6)
+        with self.assertRaises(ValueError):
+            utils.fact(-1)
     
     def test_roots(self):
-        self.assertEqual(utils.roots(1,2,1),1)
+        self.assertEqual(utils.roots(1,2,-15),(-5,3))
+        self.assertEqual(utils.roots(1,-2,1),(1))
+        self.assertEqual(utils.roots(1,0,1),())
         pass
     
     def test_integrate(self):
-        self.assertEqual(utils.integrate('x ** 2 - 1', -1, 1),1)
+        self.assertTrue(-1.34< utils.integrate('x ** 2 - 1', -1, 1) < -1.33)
         pass
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
+from math import sqrt
 
 def fact(n):
     """Computes the factorial of a natural number.
@@ -9,6 +10,13 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    if n<0:
+        raise ValueError
+    
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    return fact
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -17,8 +25,13 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
-
+    rho=b*b-4*a*c
+    if rho >0:
+        return ((-b-sqrt(rho))/(2*a),(-b+sqrt(rho))/(2*a))
+    elif rho == 0:
+        return ((-b)/(2*a))
+    else:
+        return ()
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
     
@@ -28,9 +41,16 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    pas=0.0001
+    integrale=0
+    numvalue = int((upper-lower)//pas)
+    for i in range(numvalue):
+        x=lower+i*pas
+        integrale+=(eval(function)*pas)
+    return integrale
 
 if __name__ == '__main__':
     print(fact(5))
-    print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(roots(1, 2,-15))
+    print(integrate('x**2-1', -1, 1))
+    
